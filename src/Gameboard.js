8e25af68a8aea554a4eaae1d3732072aa2b55ea3
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-04-02 13:39:07 +0300

    Create Gameboard/Cell tests and logic

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,34 +1,103 @@
 import { Ship } from './Ship.js';
-
-export class Cell {
-  #isHitted;
-  #shipRef;
-
-  constructor() {
-    this.#isHitted = false;
-    this.#shipRef = null;
-  }
-  get isHitted() {
-    return this.#isHitted;
-  }
-  get shipRef() {
-    return this.#shipRef;
-  }
-
-  setShipRef(ship) {
-    if (!(ship instanceof Ship)) {
-      throw new Error('setShipRef argument should be type of Ship');
-    }
-    this.#shipRef = ship;
-  }
-
-  setIsHitted() {
-    this.#isHitted = true;
-  }
-}
+import { Cell } from './Cell.js';
 
 export class Gameboard {
+  #board;
+  #ships;
+  #boardSize = 10;
+
   constructor() {
-    
+    this.#board = [];
+    for (let i = 0; i < this.#boardSize; i++) {
+      this.#board[i] = [];
+      for (let j = 0; j < this.#boardSize; j++) {
+        this.#board[i][j] = new Cell();
+      }
+    }
+    this.#ships = [];
+  }
+
+  get board() {
+    const boardCopy = [];
+    for (let i = 0; i < this.#boardSize; i++) {
+      boardCopy[i] = [];
+      for (let j = 0; j < this.#boardSize; j++) {
+        boardCopy[i][j] = this.#board[i][j];
+      }
+    }
+    return boardCopy;
+  }
+
+  getCell(x, y) {
+    if (x < 0 || x >= this.#boardSize || y < 0 || y >= this.#boardSize) {
+      throw new Error('Can not get a ship in unavailable postion');
+    }
+    return this.#board[x][y];
+  }
+
+  #chooseCell(i, x, y, isVertical) {
+    if (i === 0) {
+      return this.getCell(x, y);
+    } else if (isVertical) {
+      return this.getCell(i, y);
+    } else {
+      return this.getCell(x, i);
+    }
+  }
+
+  placeShip(ship, x, y, isVertical) {
+    let cell = null;
+    try {
+      for (let i = 0; i < ship.length; i++) {
+        cell = this.#chooseCell(i, x, y, isVertical);
+        if (cell.shipRef !== null) {
+          throw new Error('Can not place a ship in unavailable postion');
+        }
+      }
+    } catch (error) {
+      if (
+        error.message === 'Can not place a ship in unavailable postion' ||
+        error.message === 'Can not get a ship in unavailable postion'
+      ) {
+        throw new Error('Can not place a ship in unavailable postion');
+      }
+      throw error;
+    }
+
+    for (let i = 0; i < ship.length; i++) {
+      cell = this.#chooseCell(i, x, y, isVertical);
+      cell.setShipRef(ship);
+      this.#ships.push(ship);
+    }
+  }
+
+  receiveAttack(x, y) {
+    try {
+      const cell = this.getCell(x, y);
+      if (cell.isHitted === true) {
+        throw new Error('Can not hit unavailable postion');
+      }
+      cell.setIsHitted();
+      if (cell.shipRef !== null) {
+        cell.shipRef.hit();
+      }
+    } catch (error) {
+      if (
+        error.message === 'Can not get a ship in unavailable postion' ||
+        error.message === 'Can not hit unavailable postion'
+      ) {
+        throw new Error('Can not hit unavailable postion');
+      }
+      throw error;
+    }
+  }
+
+  isAllSunk() {
+    return this.#ships.every((ship) => {
+      if (ship.isSunk()) {
+        return true;
+      }
+      return false;
+    });
   }
 }
--- src/main.test.js
+++ src/main.test.js
@@ -1,52 +1,162 @@
 import { Ship } from './Ship.js';
 
-import { Cell } from "./Gameboard.js";
+import { Cell } from './Cell.js';
+
+import { Gameboard } from './Gameboard.js';
 
 describe('Ship tests', () => {
-  test('Ship constructor creates a proper object', () => {
-    const ship = new Ship(1);
-    expect(ship.length).toBe(1);
-  });
+  // test('Ship constructor creates a proper object', () => {
+  //   const ship = new Ship(1);
+  //   expect(ship.length).toBe(1);
+  // });
   test('Ship constructor throws an error when passing length less than 1 or higher than 4', () => {
-    const ship = new Ship(1);
-    expect(()=>{new Ship(0)}).toThrow(Error);
-    expect(()=>{new Ship(5)}).toThrow(Error);
+    expect(() => {
+      new Ship(0);
+    }).toThrow(Error);
+    expect(() => {
+      new Ship(5);
+    }).toThrow(Error);
   });
   test('Ship properly defines when it is sunk', () => {
     const ship = new Ship(1);
-    expect(ship.isSunk()).toBe(false);
+    expect(ship.isSunk()).toBeFalsy();
     ship.hit();
-    expect(ship.isSunk()).toBe(true);
+    expect(ship.isSunk()).toBeTruthy();
   });
 });
 
-describe('Gameboard tests',()=>{
-  describe('Cell tests',()=>{
-    
-    test("Cell constructor creates a proper object",()=>{
-      const cell = new Cell();
-      expect(cell.shipRef).toBeNull();
-      expect(cell.isHitted).toBe(false);
-    })
+describe('Gameboard tests', () => {
+  describe('Cell tests', () => {
+    // test("Cell constructor creates a proper object",()=>{
+    //   const cell = new Cell();
+    //   expect(cell.shipRef).toBeNull();
+    //   expect(cell.isHitted).toBe(false);
+    // })
 
-    test("shipRef setter throws an error when passing the wrong type value",()=>{
+    test('shipRef setter throws an error when passing the wrong type value', () => {
       const cell = new Cell();
-      expect(()=> {cell.setShipRef("Wrong type")}).toThrow(Error);
-      expect(()=> {cell.setShipRef(1)}).toThrow(Error);
-    })
+      expect(() => {
+        cell.setShipRef('Wrong type');
+      }).toThrow(Error);
+      expect(() => {
+        cell.setShipRef(1);
+      }).toThrow(Error);
+    });
 
-    test("shipRef setter sets ship properly",()=>{
-      const cell = new Cell();
-      const ship = new Ship(1);
-      cell.setShipRef(ship)
-      expect(cell.shipRef instanceof Ship).toBe(true);
-      expect(cell.shipRef.length).toBe(1);
-    })
+    // test("shipRef setter sets ship properly",()=>{
+    //   const cell = new Cell();
+    //   const ship = new Ship(1);
+    //   cell.setShipRef(ship)
+    //   expect(cell.shipRef instanceof Ship).toBe(true);
+    //   expect(cell.shipRef.length).toBe(1);
+    // })
 
-    test("setIsHitted marks hitted cell properly", ()=>{
-      const cell = new Cell();
-      cell.setIsHitted()
-      expect(cell.isHitted).toBe(true);
-    })
+    // test("setIsHitted marks hitted cell properly", ()=>{
+    //   const cell = new Cell();
+    //   cell.setIsHitted()
+    //   expect(cell.isHitted).toBe(true);
+    // })
   });
+
+  test('Gameboard is a 10x10 2d array with Cell as each element', () => {
+    const gameboard = new Gameboard();
+    expect(
+      gameboard.board.every((row) =>
+        row.every((element) => element instanceof Cell)
+      )
+    ).toBeTruthy();
+    expect(
+      gameboard.board.length === 10 &&
+        gameboard.board.every((row) => row.length === 10)
+    ).toBeTruthy();
+  });
+
+  test('4 cells Ship placed horizontally from 0:0 to 0:3 on the board', () => {
+    const gameboard = new Gameboard();
+    gameboard.placeShip(new Ship(4), 0, 0, false);
+    expect(
+      gameboard.board.every((row, i) =>
+        row.every((cell, j) => {
+          if (
+            (['0:0', '0:1', '0:2', '0:3'].includes(`${i}:${j}`) &&
+              gameboard.getCell(i, j).shipRef === null) ||
+            (!['0:0', '0:1', '0:2', '0:3'].includes(`${i}:${j}`) &&
+              gameboard.getCell(i, j).shipRef !== null)
+          ) {
+            return false;
+          } else {
+            return true;
+          }
+        })
+      )
+    ).toBeTruthy();
+  });
+
+  test('2 cells Ship placed vertically from 0:0 to 1:0 on the board', () => {
+    const gameboard = new Gameboard();
+    gameboard.placeShip(new Ship(2), 0, 0, true);
+    expect(
+      gameboard.board.every((row, i) =>
+        row.every((cell, j) => {
+          if (
+            (['0:0', '1:0'].includes(`${i}:${j}`) &&
+              gameboard.getCell(i, j).shipRef === null) ||
+            (!['0:0', '1:0'].includes(`${i}:${j}`) &&
+              gameboard.getCell(i, j).shipRef !== null)
+          ) {
+            return false;
+          } else {
+            return true;
+          }
+        })
+      )
+    ).toBeTruthy();
+  });
+
+  test('placeShip returns Error when placing Ship out of bounds of Gameboard', () => {
+    const gameboard = new Gameboard();
+    expect(() => {
+      gameboard.placeShip(new Ship(4), 100, 100, false);
+    }).toThrow('Can not place a ship in unavailable postion');
+  });
+
+  test('placeShip returns Error when placing Ship in position where another Ship already placed', () => {
+    const gameboard = new Gameboard();
+
+    gameboard.placeShip(new Ship(4), 0, 0, false);
+    expect(() => {
+      gameboard.placeShip(new Ship(4), 0, 0, false);
+    }).toThrow('Can not place a ship in unavailable postion');
+  });
+
+  test('receiveAttack marks Cell as hited and deals hit to a ship', () => {
+    const gameboard = new Gameboard();
+    gameboard.placeShip(new Ship(1), 0, 0, false);
+    gameboard.receiveAttack(0, 0);
+    expect(gameboard.getCell(0, 0).isHitted).toBeTruthy();
+    expect(gameboard.getCell(0, 0).shipRef.isSunk()).toBeTruthy();
+  });
+
+  test('receiveAttack returns Error when hitting unavailable postion', () => {
+    const gameboard = new Gameboard();
+    expect(() => {
+      gameboard.receiveAttack(100, 100);
+    }).toThrow('Can not hit unavailable postion');
+  });
+
+  test('receiveAttack returns Error when hitting the same postion', () => {
+    const gameboard = new Gameboard();
+    gameboard.receiveAttack(0, 0);
+    expect(() => {
+      gameboard.receiveAttack(0, 0);
+    }).toThrow('Can not hit unavailable postion');
+  });
+
+  test('isAllSunk returns proper message when all ships sunk',()=>{
+    const gameboard = new Gameboard();
+    gameboard.placeShip(new Ship(1), 0, 0, false);
+    expect(gameboard.isAllSunk()).toBeFalsy();
+    gameboard.receiveAttack(0, 0);
+    expect(gameboard.isAllSunk()).toBeTruthy();
+  })
 });
